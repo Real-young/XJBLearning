8045cb7317d9f1fc311eb58c12e087c558f912e1
booming <real_ybm@outlook.com>
2020-09-24 18:24:56 +0800

    leetcode_203

---
+++ src/booming/leetcode/leetcode_203.java
@@ -0,0 +1,25 @@
+package booming.leetcode;
+
+// 203. Remove Linked List Elements
+// https://leetcode.com/problems/remove-linked-list-elements/
+
+import com.realyoung.List;
+
+public class leetcode_203 {
+    public ListNode removeElements(ListNode head, int val) {
+        // 迭代
+        if (head == null || head.next == null) return head;
+        while (head.next != null && head.val == val) {
+            head = head.next;
+        }
+        ListNode tmp = head;
+        while (tmp.next != null) {
+            if (tmp.next.val == val) {
+                tmp.next = tmp.next.next;
+            } else {
+                tmp = tmp.next;
+            }
+        }
+        return head;
+    }
+}
