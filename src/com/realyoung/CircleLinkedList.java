0a66789e3b350df122648ba2834a7da2f839c594
booming <real_ybm@outlook.com>
2020-10-10 13:25:55 +0800

    CircleLinkedList add method

--- src/com/realyoung/CircleLinkedList.java
+++ src/com/realyoung/CircleLinkedList.java
@@ -37,12 +37,54 @@ public class CircleLinkedList<E> extends AbstractList<E> {
 
     @Override
     public void add(int index, E element) {
+        if (index == size) {
+            // 取出最后一个  然后替换
+            Node<E> oldLast = lastNode;
+            // 新插入一个节点  new 一个
+            Node<E> node = new Node<>(element, firstNode, oldLast);
+            if (oldLast == null) {
+                firstNode = lastNode;
+                firstNode.prevNode = firstNode;
+                firstNode.nextNode = firstNode;
+            } else {
+                oldLast.nextNode = node;
+                firstNode.prevNode = node;
+            }
+        } else  {
+            Node<E> nextNode = node(index);
+            Node<E> node = new Node<>(element,nextNode,nextNode.prevNode);
+            nextNode.prevNode = node;
+            nextNode.prevNode.nextNode = node;
 
+            if (nextNode == firstNode) { // index = 0 的时候
+                firstNode = node;
+            }
+
+        }
+
+        size ++;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+
+        Node<E> node = node(index);
+        Node<E> next = node.nextNode;
+        Node<E> prev = node.prevNode;
+        if (prev == null) {
+            firstNode = next;
+        } else {
+            prev.nextNode = next;
+        }
+
+        if (next == null) {
+            lastNode = prev;
+        } else {
+            next.prevNode = prev;
+        }
+        size --;
+
+        return node.element;
     }
 
     @Override
@@ -79,5 +121,19 @@ public class CircleLinkedList<E> extends AbstractList<E> {
             return node;
         }
 
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        string.append("size = ").append(size).append(", [");
+        Node<E> node = firstNode;
+        for(int i = 0; i < size; i++) {
+            if (i!=0) string.append(", ");
+            string.append(node.element);
+            node = node.nextNode;
+        }
+        string.append("]");
+        return string.toString();
     }
 }
