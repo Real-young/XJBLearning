290fe43b24346b4eaa886dfb07c3b9841719c4ac
booming <real_ybm@outlook.com>
2020-10-19 11:53:21 +0800

    feat: leetcode_20 by HashMap

--- src/booming/leetcode/leetcode_20.java
+++ src/booming/leetcode/leetcode_20.java
@@ -5,22 +5,57 @@ package booming.leetcode;
 
 import stack.Stack;
 
+import java.util.HashMap;
+
 public class leetcode_20 {
+
+//    private HashMap<Character, Character> map = new HashMap<>();
+//
+//    public leetcode_20() {
+//        map.put('(',')');
+//        map.put('[',']');
+//        map.put('{','}');
+//    }
+
+    private static HashMap<Character, Character> map = new HashMap<>();
+
+    static {
+        map.put('(',')');
+        map.put('[',']');
+        map.put('{','}');
+    }
+
+
     public boolean isValid(String s) {
+//        Stack<Character> stack = new Stack<>();
+//        int length = s.length();
+//        for (int i = 0; i < length; i++) {
+//            char c = s.charAt(i);
+//            if (c == '(' || c == '[' || c == '{') {
+//                stack.push(c);
+//            } else {
+//                if (stack.isEmpty()) return false;
+//                char left = stack.pop();
+//                if (left == '(' && c != ')') return false;
+//                if (left == '[' && c != ']') return false;
+//                if (left == '{' && c != '}') return false;
+//            }
+//
+//        }
+//        return stack.isEmpty();
+
+
         Stack<Character> stack = new Stack<>();
+
         int length = s.length();
         for (int i = 0; i < length; i++) {
             char c = s.charAt(i);
-            if (c == '(' || c == '[' || c == '{') {
+            if (map.containsKey(c)) {
                 stack.push(c);
             } else {
                 if (stack.isEmpty()) return false;
-                char left = stack.pop();
-                if (left == '(' && c != ')') return false;
-                if (left == '[' && c != ']') return false;
-                if (left == '{' && c != '}') return false;
+                if (map.get(stack.pop()) != c) return false;
             }
-
         }
         return stack.isEmpty();
     }
