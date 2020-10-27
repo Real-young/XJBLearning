41004441a38bfeb3a27d18f56bfc796293ee94a0
booming <real_ybm@outlook.com>
2020-10-27 15:23:10 +0800

    feat: BinarySearchTree

---
+++ src/com/realyoungtree/BinarSearchTree.java
@@ -0,0 +1,84 @@
+package com.realyoungtree;
+
+// 二叉搜索树
+
+public class BinarSearchTree<E> {
+
+    private int size;
+    private Node<E> root;
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public void add(E element) {
+        elementNotNullCheck(element);
+        if (root == null) {
+            root = new Node<>(element, null);
+            size ++;
+            return;
+        }
+
+        // 添加的不是第一个节点
+        Node<E> node = root;
+        Node<E> parent = root;
+        int cmp = 0;
+        while (node != null) {
+            cmp = compare(element, node.element);
+            parent = node;
+            if (cmp > 0) {
+                node = node.right;
+            } else if (cmp < 0) {
+                node = node.left;
+            } else { // 相等
+                return;
+            }
+        }
+
+        // 查看添加到哪个位置
+        Node<E> newNode = new Node<>(element, parent);
+        if (cmp > 0) {
+            parent.right = newNode;
+        } else {
+            parent.left = newNode;
+        }
+        size ++;
+    }
+
+    public void remove(E element) {
+
+    }
+
+    public boolean contains(E element) {
+        return false;
+    }
+
+    private void elementNotNullCheck(E element) {
+        if (element == null) {
+            throw new IllegalArgumentException("element must not be null");
+        }
+    }
+
+    private static class Node<E> {
+        E element;
+        Node<E> left;
+        Node<E> right;
+        Node<E> parent;
+        public Node(E element, Node<E> parent) {
+
+        }
+    }
+
+    private int compare(E e1, E e2) {
+        return 0;
+    }
+
+}
---
+++ src/com/realyoungtree/Main.java
@@ -0,0 +1,7 @@
+package com.realyoungtree;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
