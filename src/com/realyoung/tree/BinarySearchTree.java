bc99d25666bb697792dcddaca65a781660b7f6ff
booming <real_ybm@outlook.com>
2020-10-29 15:04:39 +0800

    test: printf BinarySearchTree

---
+++ src/com/realyoung/tree/BinarySearchTree.java
@@ -0,0 +1,140 @@
+package com.realyoung.tree;
+
+// 二叉搜索树
+
+import com.realyoung.printer.BinaryTreeInfo;
+
+import java.util.Comparator;
+
+public class BinarySearchTree<E> implements BinaryTreeInfo {
+
+    private int size;
+    private Node<E> root;
+    // 比较器
+    private Comparator<E> comparator;
+
+    public BinarySearchTree() {
+        this(null);
+    }
+
+    public BinarySearchTree(Comparator<E> comparator) {
+        this.comparator = comparator;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public void add(E element) {
+        elementNotNullCheck(element);
+        if (root == null) {
+            root = new Node<>(element, null);
+            size ++;
+            return;
+        }
+
+        // 添加的不是第一个节点
+        Node<E> node = root;
+        Node<E> parent = root;
+        int cmp = 0;
+        while (node != null) {
+            cmp = compare(element, node.element);
+            parent = node;
+            if (cmp > 0) {
+                node = node.right;
+            } else if (cmp < 0) {
+                node = node.left;
+            } else { // 相等
+                node.element = element; // 覆盖
+                return;
+            }
+        }
+
+        // 查看添加到哪个位置
+        Node<E> newNode = new Node<>(element, parent);
+        if (cmp > 0) {
+            parent.right = newNode;
+        } else {
+            parent.left = newNode;
+        }
+        size ++;
+    }
+
+    public void remove(E element) {
+
+    }
+
+    public boolean contains(E element) {
+        return false;
+    }
+
+    private void elementNotNullCheck(E element) {
+        if (element == null) {
+            throw new IllegalArgumentException("element must not be null");
+        }
+    }
+
+    @Override
+    public Object root() {
+        return root;
+    }
+
+    @Override
+    public Object left(Object node) {
+        return ((Node<E>)node).left;
+    }
+
+    @Override
+    public Object right(Object node) {
+        return ((Node<E>)node).right;
+    }
+
+    @Override
+    public Object string(Object node) {
+        return ((Node<E>)node).element;
+    }
+
+    private static class Node<E> {
+        E element;
+        Node<E> left;
+        Node<E> right;
+        Node<E> parent;
+        public Node(E element, Node<E> parent) {
+            this.element = element;
+            this.parent = parent;
+        }
+    }
+
+
+    /*
+     *  @return 返回值等于0，代表 e1 e2 相等
+     *               大于0， e1 > e2
+     *               小于0， e1 < e2
+     * */
+    private int compare(E e1, E e2) {
+
+        // 用比较器做比较 可选择对象的值等
+        if (comparator != null) {
+            return comparator.compare(e1, e2);
+        }
+        /*
+         * 强转
+         * 如果没有比较器 则遵循接口
+         * 强制对象实现 compareTo
+         * */
+        return ((Comparable<E>)e1).compareTo(e2);
+    }
+
+
+
+    }
+
+}
--- src/com/realyoung/tree/Main.java
+++ src/com/realyoung/tree/Main.java
@@ -1,7 +1,31 @@
 package com.realyoung.tree;
 
+import com.realyoung.printer.BinaryTrees;
+//import com.realyoung.BinarySearchTree.Visitor;
+//import com.realyoung.file.Files;
+import com.realyoung.printer.BinaryTreeInfo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("helloworld");
+
+        test1();
+
     }
+
+    static void test1() {
+        Integer data[] = new Integer[] {
+                7, 4, 9, 2, 5, 8, 11, 3, 12, 1
+        };
+
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        for (int i = 0; i < data.length; i++) {
+            bst.add(data[i]);
+        }
+
+        BinaryTrees.println(bst);
+
+
+    }
+
+
 }
